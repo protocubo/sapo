ffb101a8eefe916b2495d4d24fa5d10ff03a1770
Jonas Malaco Filho <jonas@jonasmalaco.com>
2016-02-22 15:50:38 -0300

    Sapo: /ticket -> /ticket?ofUser=_

--- src/sapo/Routes.hx
+++ src/sapo/Routes.hx
@@ -5,8 +5,11 @@ import neko.Web;
 import sapo.Spod;
 
 class TinkRoutes {
-	public function doTicket()
-		Sys.println(sapo.view.Ticket.render());
+	public function doTickets(?args:{ ?ofUser:User })
+	{
+		if (args == null) args = {};
+		Sys.println(sapo.view.Tickets.render(args.ofUser));
+	}
 		
 	public function doLogin()
 		Sys.println(sapo.view.Login.render());
@@ -20,11 +23,11 @@ class TinkRoutes {
 	public function doPayment()
 		Sys.println(sapo.view.Payment.render());
 		
-	public function doSurveys()
-		Sys.println(sapo.view.Surveys.render());
+	// public function doSurveys()
+	// 	Sys.println(sapo.view.Surveys.render());
 	
-	public function doSurvey(s: sapo.Survey)
-		Sys.println(sapo.view.Survey.render(s));
+	// public function doSurvey(s:sapo.Survey)
+	// 	Sys.println(sapo.view.Survey.render(s));
 
 	public function new() {}
 }
