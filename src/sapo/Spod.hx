c26c9e42b9f55ee638dd40b681e612482a0118fd
Jonas Malaco Filho <jonas@jonasmalaco.com>
2016-02-27 14:36:47 -0300

    Minor

--- src/sapo/Spod.hx
+++ src/sapo/Spod.hx
@@ -1,11 +1,19 @@
 package sapo;
 
+// this module has been deprecated, use sapo.spod.* instead
+
+// in sapo.spod.User:
+
 typedef User = sapo.spod.User;
 typedef Group = sapo.spod.User.Group;
 typedef Session = sapo.spod.User.Session;
 
+// in sapo.spod.Ticket:
+
 typedef Ticket = sapo.spod.Ticket;
 typedef TicketMessage = sapo.spod.Ticket.TicketMessage;
+
+// in sapo.spod.Other:
 
 typedef SurveyStatus = sapo.spod.Other.SurveyStatus;
 typedef TicketStatus = sapo.spod.Other.TicketStatus;
