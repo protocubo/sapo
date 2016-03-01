743326a2dcc0f749396d7ff018787493af4b1f38
Caio <caio.honma@gmail.com>
2016-03-01 20:43:23 -0300

    Summary Default Working ... TODO: History + filters?

--- src/sapo/route/SummaryRoutes.hx
+++ src/sapo/route/SummaryRoutes.hx
@@ -1,4 +1,5 @@
 package sapo.route;
+import common.db.MoreTypes.HaxeTimestamp;
 import common.db.MoreTypes.Privilege;
 import neko.Web;
 import sapo.Context;
@@ -11,6 +12,8 @@ import sys.db.Manager;
  * @author Caio
  */
 using Lambda;
+using sapo.route.SummaryRoutes.SummaryTools;
+
 class SummaryRoutes extends AccessControl
 {
 	//TODO: Pegar params de filtro
@@ -39,23 +42,28 @@ class SummaryRoutes extends AccessControl
 			else
 				status = PesqStatus.Completa;
 			
-			group.set(c.group, status);
+			group.set(c.grupo, status);
 			userCheck.set(c.user, group);				
 		}
 		
 		//Map do tipo data; categoria; val que vai para a view (para todos fazer Completas, pendentes e recusadas + os params adicionais (grupo ou usuários)
-		var dateVal : Map<Date, Map<String, Int>> = new Map();
+		var dateVal : Map<String, Map<String, Int>> = new Map();
 		
 		var resultsQuery = Manager.cnx.request("SELECT s.user_id as user, s.`group` as grupo,STRFTIME('%Y-%m-%d', s.date_finished) as date_end , COUNT(*) as pesqGrupo,  SUM( CASE WHEN checkSupervisor IS NULL THEN 1 ELSE 0 END) as nullSupervisor, SUM( CASE WHEN checkCT IS NULL THEN 1 ELSE 0 END) as nullCT, SUM(CASE WHEN checkSuper IS NULL THEN 1 ELSE 0 END) AS nullSuper FROM Survey s JOIN UpdatedSurvey us ON s.old_survey_id = us.old_survey_id AND s.syncTimestamp = us.syncTimestamp WHERE s.syncTimestamp > 1000 GROUP BY s.user_id, s.`group`, date_end ORDER BY s.user_id, s.`group`, date_end").results();
-		var header = ["Data", "Supervisor", "CT", "Super", "Completa", "Recusadas", "Aceitas"];
+		var header = ["Data", "Supervisor", "CT", "Super", "Completas", "Recusadas", "Aceitas"];
 		for (r in resultsQuery)
 		{
-			var date = Date.fromString(r.date_end);
+			if (r.date_end == null)
+				continue;
+			trace(r.date_end);
+			var date = r.date_end;
 			var curDateHash : Map<String,Int> = new Map();
 			if (dateVal.exists(date))
 				curDateHash = dateVal.get(date);
-			
-			var mode = userCheck.get(r.user_id).get(r.group);
+
+			var mode = userCheck.get(r.user).get(r.grupo);
+			//trace(userCheck.get(r.user);
+				
 			switch(mode)
 			{
 				//Obrigatoriamente todos responderam...n sobe barra, sobem os controles
@@ -67,19 +75,20 @@ class SummaryRoutes extends AccessControl
 					curDateHash.set("Recusadas", curval + r.pesqGrupo);
 				case PesqStatus.Pendente:
 					if (r.nullCT == r.pesqGrupo)
-						curDateHash.set("CT", curDateHash.get("CT") + r.pesqGrupo);
-					curDateHash.set("Supervisor",curDateHash.get("Supervisor") +  r.pesqGrupo);
-					curDateHash.set("Super", curDateHash.get("Super") + r.pesqGrupo);
+						curDateHash.set("CT", curDateHash.get("CT").getVal() + r.pesqGrupo);
+					curDateHash.set("Supervisor",curDateHash.get("Supervisor").getVal() +  r.pesqGrupo);
+					curDateHash.set("Super", curDateHash.get("Super").getVal() + r.pesqGrupo);
 				case PesqStatus.Completa:
-					curDateHash.set("Completas", curDateHash.get("Completas") + r.pesqGrupo);
+					curDateHash.set("Completas", curDateHash.get("Completas").getVal() + r.pesqGrupo);
 					if (r.nullCT == r.pesqGrupo)
-						curDateHash.set("CT", curDateHash.get("CT") + r.pesqGrupo);
-					curDateHash.set("Supervisor",curDateHash.get("Supervisor") +  r.pesqGrupo);
-					curDateHash.set("Super", curDateHash.get("Super") + r.pesqGrupo);
+						curDateHash.set("CT", curDateHash.get("CT").getVal() + r.pesqGrupo);
+					curDateHash.set("Supervisor",curDateHash.get("Supervisor").getVal() +  r.pesqGrupo);
+					curDateHash.set("Super", curDateHash.get("Super").getVal() + r.pesqGrupo);
 			}
 			
-			dateVal.set(Date.fromString(r.date_end), curDateHash);
+			dateVal.set(r.date_end, curDateHash);
 		}
+		trace(dateVal);
 		
 		Sys.println(sapo.view.Summary.render(dateVal, header));
 	}
@@ -89,6 +98,17 @@ class SummaryRoutes extends AccessControl
 		
 	}
 	
+
+	
+	
+}
+
+class SummaryTools
+{
+	public static function getVal(v : Int) : Int
+	{
+		return v != null ? v : 0;
+	}
 }
 
 enum PesqStatus {
