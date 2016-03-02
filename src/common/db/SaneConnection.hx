b617d19a1bb0c689ad6d215ee625ae77a4f9d830
Jonas Malaco Filho <jonas@jonasmalaco.com>
2016-03-02 17:30:11 -0300

    Minor

--- src/common/db/SaneConnection.hx
+++ src/common/db/SaneConnection.hx
@@ -4,7 +4,6 @@ import sys.db.Connection;
 
 @:forward
 abstract SaneConnection(Connection) from Connection {
-	public inline function startTransaction() this.request("BEGIN");
 	public inline function commit() this.request("COMMIT");
 	public inline function rollback() this.request("ROLLBACK");
 }
