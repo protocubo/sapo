d6776d07ee2c156cb12335b1fcb60acf9179082d
Caio <caio.honma@gmail.com>
2016-03-01 21:08:29 -0300

    Already made the production query (commented for convenience)

--- src/sapo/route/SummaryRoutes.hx
+++ src/sapo/route/SummaryRoutes.hx
@@ -21,6 +21,11 @@ class SummaryRoutes extends AccessControl
 	public function doDefault()
 	{
 		//Todos os estados atuais da pesquisa por grupo
+		
+		//TODO:Uncomment this line
+		//var controlResults = Manager.cnx.request("SELECT s.user_id as user, s.`group` as grupo, COUNT(*) as pesqGrupo, SUM(CASE WHEN (checkSupervisor IS NULL OR checkCT IS NULL OR checkSuper IS NULL) OR ((checkSuperVisor IS NOT NULL AND checkSupervisor != 0 AND checkCT IS NOT NULL AND checkCT != 0 AND checkSuper IS NOT NULL AND checkSuper != 0) AND NOT (checkSupervisor = 1  AND checkCT = 1 AND checkSuper = 1) ) THEN 1 ELSE 0 END) as Completa, SUM(CASE WHEN checkSupervisor = 0 AND checkCT = 0 AND checkSuper = 0 THEN 1 ELSE 0 END) as allFalse, SUM(CASE WHEN checkSupervisor = 0 OR checkCT = 0 OR checkSuper = 0 THEN 1 ELSE 0 END) as hasFalse, SUM(CASE WHEN checkSupervisor = 1 AND checkCT = 1 AND checkSuper = 1 THEN 1 ELSE 0 END) as isTrue, SUM( CASE WHEN checkSupervisor IS NULL THEN 1 ELSE 0 END) as nullSupervisor, SUM( CASE WHEN checkCT IS NULL THEN 1 ELSE 0 END) as nullCT, SUM(CASE WHEN checkSuper IS NULL THEN 1 ELSE 0 END) AS nullSuper FROM Survey s JOIN UpdatedSurvey us ON s.old_survey_id = us.old_survey_id AND s.syncTimestamp = us.syncTimestamp WHERE s.syncTimestamp > "+ DateTools.delta(Date.now(), -1000.0*60*60*24*28).getTime() +" GROUP BY s.user_id, s.`group` ORDER BY s.user_id, s.`group`").results();
+		
+		//TODO: Comment this line
 		var controlResults = Manager.cnx.request("SELECT s.user_id as user, s.`group` as grupo, COUNT(*) as pesqGrupo, SUM(CASE WHEN (checkSupervisor IS NULL OR checkCT IS NULL OR checkSuper IS NULL) OR ((checkSuperVisor IS NOT NULL AND checkSupervisor != 0 AND checkCT IS NOT NULL AND checkCT != 0 AND checkSuper IS NOT NULL AND checkSuper != 0) AND NOT (checkSupervisor = 1  AND checkCT = 1 AND checkSuper = 1) ) THEN 1 ELSE 0 END) as Completa, SUM(CASE WHEN checkSupervisor = 0 AND checkCT = 0 AND checkSuper = 0 THEN 1 ELSE 0 END) as allFalse, SUM(CASE WHEN checkSupervisor = 0 OR checkCT = 0 OR checkSuper = 0 THEN 1 ELSE 0 END) as hasFalse, SUM(CASE WHEN checkSupervisor = 1 AND checkCT = 1 AND checkSuper = 1 THEN 1 ELSE 0 END) as isTrue, SUM( CASE WHEN checkSupervisor IS NULL THEN 1 ELSE 0 END) as nullSupervisor, SUM( CASE WHEN checkCT IS NULL THEN 1 ELSE 0 END) as nullCT, SUM(CASE WHEN checkSuper IS NULL THEN 1 ELSE 0 END) AS nullSuper FROM Survey s JOIN UpdatedSurvey us ON s.old_survey_id = us.old_survey_id AND s.syncTimestamp = us.syncTimestamp WHERE s.syncTimestamp > 1000 GROUP BY s.user_id, s.`group` ORDER BY s.user_id, s.`group`").results();
 		
 		//User;group;status - 
@@ -49,7 +54,11 @@ class SummaryRoutes extends AccessControl
 		//Map do tipo data; categoria; val que vai para a view (para todos fazer Completas, pendentes e recusadas + os params adicionais (grupo ou usuários)
 		var dateVal : Map<String, Map<String, Int>> = new Map();
 		
+		//TODO: Uncomment this line
+		//var resultsQuery = Manager.cnx.request("SELECT s.user_id as user, s.`group` as grupo,STRFTIME('%Y-%m-%d', s.date_finished) as date_end , COUNT(*) as pesqGrupo,  SUM( CASE WHEN checkSupervisor IS NULL THEN 1 ELSE 0 END) as nullSupervisor, SUM( CASE WHEN checkCT IS NULL THEN 1 ELSE 0 END) as nullCT, SUM(CASE WHEN checkSuper IS NULL THEN 1 ELSE 0 END) AS nullSuper FROM Survey s JOIN UpdatedSurvey us ON s.old_survey_id = us.old_survey_id AND s.syncTimestamp = us.syncTimestamp WHERE s.syncTimestamp > "+DateTools.delta(Date.now(), -1000.0*60*60*24*28).getTime()+" GROUP BY s.user_id, s.`group`, date_end ORDER BY s.user_id, s.`group`, date_end").results();
+		//TODO: Comment this line
 		var resultsQuery = Manager.cnx.request("SELECT s.user_id as user, s.`group` as grupo,STRFTIME('%Y-%m-%d', s.date_finished) as date_end , COUNT(*) as pesqGrupo,  SUM( CASE WHEN checkSupervisor IS NULL THEN 1 ELSE 0 END) as nullSupervisor, SUM( CASE WHEN checkCT IS NULL THEN 1 ELSE 0 END) as nullCT, SUM(CASE WHEN checkSuper IS NULL THEN 1 ELSE 0 END) AS nullSuper FROM Survey s JOIN UpdatedSurvey us ON s.old_survey_id = us.old_survey_id AND s.syncTimestamp = us.syncTimestamp WHERE s.syncTimestamp > 1000 GROUP BY s.user_id, s.`group`, date_end ORDER BY s.user_id, s.`group`, date_end").results();
+		
 		var header = ["Data", "Supervisor", "CT", "Super", "Completas", "Recusadas", "Aceitas"];
 		for (r in resultsQuery)
 		{
