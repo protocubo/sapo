583d61187d1858d85b9182adac5b2bca984f1734
Arthur Szasz <arthur@elebeta.com.br>
2016-03-02 20:48:35 -0300

    Insert @tt in EnumSPOD

--- src/common/spod/EnumSPOD.hx
+++ src/common/spod/EnumSPOD.hx
@@ -20,15 +20,14 @@ import sys.db.Types.SString;
  }
  
  //Classes de Relação com id/desc apenas
- @dbNullVal(0) enum AguaEncanada {
-	@dbVal(1) Sim;
-	@dbVal(2) SimPropriedade;
-	@dbVal(3) Nao;
-}
+ @dbNullVal(0) @tt("Recebe Água Encanada?") enum AguaEncanada {
+	@dbVal(1) @tt("Sim, pelo menos em um cômodo.") Sim;
+	@dbVal(2) @tt("Sim, na propriedade.")  SimPropriedade;
+	@dbVal(3) @tt("Não")  Nao;
+ }
+ class AguaEncanada_Tbl extends EnumTable { }
 
-class AguaEncanada_Tbl extends EnumTable {}
-
- @dbNullVal(101) @tempstring("Ano de fabricação do veículo mais novo:") enum AnoVeiculoMaisRecente {
+ @dbNullVal(101) @tempstring("Veículo:") enum AnoVeiculoMaisRecente {
 	@dbVal(1) @tt("Posterior a 2014") Mais2014;
 	@dbVal(2) @tt("De 2010 a 2013") De2010;
 	@dbVal(3) @tt("De 2005 a 2009") De2005;
@@ -38,130 +37,112 @@ class AguaEncanada_Tbl extends EnumTable {}
 	@dbVal(7) @tt("Anterior a 1989") De1989;
 	@dbVal(90) @tt("Não sabe") NaoSabe;
 	@dbVal(101) @tt("Não respondeu") NaoRespondeu;
-}
-
- class AnoVeiculoMaisRecente_Tbl extends EnumTable
-{}
-
-@dbNullVal(8) enum FrequenciaViagem {
-	@dbVal(1) Raramente;
-	@dbVal(2) Menos1Mes;
-	@dbVal(3) Vez1Mes;
-	@dbVal(4) Vez2Mes;
-	@dbVal(5) Semana1a2;
-	@dbVal(6) Semana3a4;
-	@dbVal(7) MaisSemana5;
-	@dbVal(8) NaoRespondeu;
  }
- 
-//@:keep class FrequenciaViagem_Tbl extends EnumTable {}
+ class AnoVeiculoMaisRecente_Tbl extends EnumTable { }
+
+@dbNullVal(8) @tt("Frequência  com que faz a viagem:") enum FrequenciaViagem {
+	@dbVal(1) @tt("Raramente") Raramente;
+	@dbVal(2) @tt("Menos de 1 vez por mês") Menos1Mes;
+	@dbVal(3) @tt("Até 1 vez por mês") Vez1Mes;
+	@dbVal(4) @tt("Até 2 vezes por mês") Vez2Mes;
+	@dbVal(5) @tt("De 1 a 2 vezes por semana") Semana1a2;
+	@dbVal(6) @tt("De 3 a 4 vezes por semana") Semana3a4;
+	@dbVal(7) @tt("Mais de 5 vezes por semana") MaisSemana5;
+	@dbVal(8) @tt("Não Respondeu") NaoRespondeu;
+ }
+//@:keep class FrequenciaViagem_Tbl extends EnumTable { }
   
-@dbNullVal(9) enum TempoPermanenciaEstacionamento {
-	 @dbVal(1) MeiaHora;
-	 @dbVal(2) UmaHora;
-	 @dbVal(3) DuasHoras;
-	 @dbVal(4) Turno;
-	 @dbVal(5) Diaria;
-	 @dbVal(6) Pernoite;
-	 @dbVal(7) MaisDeUmDia;
-	 @dbVal(8) NaoSabe;
-	 @dbVal(9) NaoRespondeu;
+@dbNullVal(9) @tt("Tempo de Permanência no Estacionamento:") enum TempoPermanenciaEstacionamento {
+	 @dbVal(1) @tt("Meia hora") MeiaHora;
+	 @dbVal(2) @tt("Uma hora") UmaHora;
+	 @dbVal(3) @tt("Duas horas") DuasHoras;
+	 @dbVal(4) @tt("Um turno") Turno;
+	 @dbVal(5) @tt("Um dia") Diaria;
+	 @dbVal(6) @tt("Pernoite") Pernoite;
+	 @dbVal(7) @tt("Mais de um dia") MaisDeUmDia;
+	 @dbVal(8) @tt("Não Sabe") NaoSabe;
+	 @dbVal(9) @tt("Não Respondeu") NaoRespondeu;
  }
- 
-//  class TempoPermanenciaEstacionamento_Tbl extends EnumTable {}
+//  class TempoPermanenciaEstacionamento_Tbl extends EnumTable { }
 
-@dbNullVal(101) enum AtividadeMorador
+@dbNullVal(101) @tt("Principal Atividade:")enum AtividadeMorador
 {
-	@dbVal(1) EmpresaPrivada;
-	@dbVal(2) Publico;
-	@dbVal(3) Liberal;
-	@dbVal(4) Empresario;
-	@dbVal(5) Informal;
-	@dbVal(6) Domestico;
-	@dbVal(7) Voluntario;
-	@dbVal(8) Lar;
-	@dbVal(9) Aposentado;
-	@dbVal(10) Desempregado;
-	@dbVal(11) EstudanteRegular;
-	@dbVal(12) EstudanteOutros;
-	@dbVal(13) NaoTem;
-	@dbVal(99) Outros;
-	@dbVal(101) NaoRespondeu;
-}
-
- class AtividadeMorador_Tbl extends EnumTable
- {}
+	@dbVal(1)@tt("Empregado de Empresa Privada")  EmpresaPrivada;
+	@dbVal(2) @tt("Funcionário Público") Publico;
+	@dbVal(3) @tt("Profissional Liberal") Liberal;
+	@dbVal(4) @tt("Empresário") Empresario;
+	@dbVal(5) @tt("Trabalho Informal") Informal;
+	@dbVal(6) @tt("Empregado Doméstico") Domestico;
+	@dbVal(7) @tt("Voluntário") Voluntario;
+	@dbVal(8) @tt("Cuidar do próprio Lar") Lar;
+	@dbVal(9) @tt("Aposentado") Aposentado;
+	@dbVal(10) @tt("Desempregado") Desempregado;
+	@dbVal(11) @tt("Estudante Regular") EstudanteRegular;
+	@dbVal(12) @tt("Estudante Outros") EstudanteOutros;
+	@dbVal(13) @tt("Não tem") NaoTem;
+	@dbVal(99) @tt("Outros") Outros;
+	@dbVal(101) @tt("Não Respondeu") NaoRespondeu;
+ }
+ class AtividadeMorador_Tbl extends EnumTable{ }
 	
- @dbNullVal(101) enum CondicaoMoradia {
-	@dbVal(1) Propria;
-	@dbVal(2) PropriaAquisicao;
-	@dbVal(3) Alugada;
-	@dbVal(4) Cedida;
-	@dbVal(5) Funcional;
-	@dbVal(6) ConcessaoUso;
-	@dbVal(99) Outros;
-	@dbVal(101) NaoRespondeu;
-}
-  class CondicaoMoradia_Tbl extends EnumTable{}
- 
-  @dbNullVal(5) enum EmpregadosDomesticos {
-	@dbVal(1) Residente;
-	@dbVal(2) Mensalista;
-	@dbVal(3) Diarista;
-	@dbVal(4) NaoPossui;
-	@dbVal(5) NaoRespondeu;
+ @dbNullVal(101) @tt("Moradia:") enum CondicaoMoradia {
+	@dbVal(1) @tt("Própria") Propria;
+	@dbVal(2) @tt("Própria, em aquisicao") PropriaAquisicao;
+	@dbVal(3) @tt("Alugada") Alugada;
+	@dbVal(4) @tt("Cedida") Cedida;
+	@dbVal(5) @tt("Funcional") Funcional;
+	@dbVal(6) @tt("Concessão de uso") ConcessaoUso;
+	@dbVal(99) @tt("Outros") Outros;
+	@dbVal(101) @tt("Não Respondeu") NaoRespondeu;
  }
+ class CondicaoMoradia_Tbl extends EnumTable{ }
  
- class EmpregadosDomesticos_Tbl extends EnumTable {
-	 
+  @dbNullVal(5) @tt("Empregados Doméstico:") enum EmpregadosDomesticos {
+	@dbVal(1) @tt("Residente") Residente;
+	@dbVal(2) @tt("Mensalista") Mensalista;
+	@dbVal(3) @tt("Diarista") Diarista;
+	@dbVal(4) @tt("Não Possui") NaoPossui;
+	@dbVal(5) @tt("Não Respondeu") NaoRespondeu;
  }
+ class EmpregadosDomesticos_Tbl extends EnumTable { }
    
 
- @dbNullVal(0) enum EstadoPesquisa {
-	@dbVal(1) Concluida;
-	@dbVal(2) IncompletaMorador;
-	@dbVal(3) IncompletaViagem;
+ @dbNullVal(0) @tt("Status da Pesquisa:") enum EstadoPesquisa {
+	@dbVal(1) @tt("Concluída") Concluida;
+	@dbVal(2) @tt("Incompleta, falta entrevistar morador") IncompletaMorador;
+	@dbVal(3) @tt("Incompleta, falta registrar viagem") IncompletaViagem;
  }
-  class EstadoPesquisa_Tbl extends EnumTable {
-	 
- }
+ class EstadoPesquisa_Tbl extends EnumTable { }
+
  
-  @dbNullVal(101) enum FormaPagamento {
-	@dbVal(1) Dinheiro;
-	@dbVal(2) Cartao;
-	@dbVal(3) VT;
-	@dbVal(4) Estudante;
-	@dbVal(5) Gratuidade;
-	@dbVal(99) Outros;
-	@dbVal(101) NaoRespondeu;
+  @dbNullVal(101) @tt("Forma de Pagamento") enum FormaPagamento {
+	@dbVal(1) @tt("Dinheiro") Dinheiro;
+	@dbVal(2) @tt("Cartão") Cartao;
+	@dbVal(3) @tt("VT") VT;
+	@dbVal(4) @tt("Estudante") Estudante;
+	@dbVal(5) @tt("Gratuidade") Gratuidade;
+	@dbVal(99) @tt("Outros") Outros;
+	@dbVal(101) @tt("Não Respondeu") NaoRespondeu;
  }
- 
-  class FormaPagamento_Tbl extends EnumTable {
-	 
- }
- 
+ class FormaPagamento_Tbl extends EnumTable { }
    
-  @dbNullVal(99) enum GrauInstrucao
- {
-	 @dbVal(1) Analfabeto;
-	 @dbVal(2) Alfabetizado;
-	 @dbVal(3) PreEscolar;
-	 @dbVal(4) FundamentalIncompleto;
-	 @dbVal(5) FundamentalCompleto;
-	 @dbVal(6) MedioIncompleto;
-	 @dbVal(7) MedioCompleto;
-	 @dbVal(8) SuperiorIncompleto;
-	 @dbVal(9) SuperiorCompleto;
-	 @dbVal(10) Pos;
-	 @dbVal(11) NaoEstuda;
-	 @dbVal(99) NaoRespondeu;
+  @dbNullVal(99) enum GrauInstrucao {
+	 @dbVal(1) @tt("Analfabeto") Analfabeto;
+	 @dbVal(2) @tt("Alfabetizado") Alfabetizado;
+	 @dbVal(3) @tt("Pré-Escolar") PreEscolar;
+	 @dbVal(4) @tt("Fundamental Incompleto") FundamentalIncompleto;
+	 @dbVal(5) @tt("Fundamental Completo") FundamentalCompleto;
+	 @dbVal(6) @tt("Médio Incompleto") MedioIncompleto;
+	 @dbVal(7) @tt("Ensino Médio Completo") MedioCompleto;
+	 @dbVal(8) @tt("Superior Incompleto") SuperiorIncompleto;
+	 @dbVal(9) @tt("Superior Completo") SuperiorCompleto;
+	 @dbVal(10) @tt("Pós-Graduação") Pos;
+	 @dbVal(11) @tt("Não Estuda") NaoEstuda;
+	 @dbVal(99) @tt("Não Respondeu") NaoRespondeu;
  }
+ class GrauInstrucao_Tbl extends EnumTable { }
  
-  class GrauInstrucao_Tbl extends EnumTable {
-	 
- }
- 
-  @dbNullVal(101) enum Idade
+  @dbNullVal(101) @tt("Idade") enum Idade
  {
 	@dbVal(1) @tt("0 - 4") De00Ate04;
 	@dbVal(2) @tt("5 - 9") De05Ate09;
@@ -178,196 +159,156 @@ class AguaEncanada_Tbl extends EnumTable {}
 	@dbVal(13) @tt(" 80 - 120") Mais80;
 	@dbVal(101) @tt(" N/I ") NaoRespondeu;
  }
- 
-  class Idade_Tbl extends EnumTable {
-	 
- }
+ class Idade_Tbl extends EnumTable { }
 
-
- 
-  @dbNullVal(0) enum MeioTransporte{
-	 @dbVal(1) Ape;
-	 @dbVal(2) Bicicleta;
-	 @dbVal(3) OnibusConv;
-	 @dbVal(4) BRT;
-	 @dbVal(5) Escolar;
-	 @dbVal(6) Fretado;
-	 @dbVal(7) Clandestino;
-	 @dbVal(8) Metro;
-	 @dbVal(9) AutoCond;
-	 @dbVal(10) AutoPass;
-	 @dbVal(11) MotoCond;
-	 @dbVal(12) MotoPass;
-	 @dbVal(13) Taxi;
-	 @dbVal(14) MotoristaPrivado;
-	 @dbVal(15) MotoTaxi;
-	 @dbVal(99) Outros;
+  @dbNullVal(0) @tt("Meio de Transporte") enum MeioTransporte{
+	 @dbVal(1) @tt("A pé") Ape;
+	 @dbVal(2) @tt("Bicicleta") Bicicleta;
+	 @dbVal(3) @tt("Ônibusi Convencional") OnibusConv;
+	 @dbVal(4) @tt("BRT") BRT;
+	 @dbVal(5) @tt("Escolar") Escolar;
+	 @dbVal(6) @tt("Fretado") Fretado;
+	 @dbVal(7) @tt("Clandestino") Clandestino;
+	 @dbVal(8) @tt("Metrô") Metro;
+	 @dbVal(9) @tt("Automóvel - Condutor") AutoCond;
+	 @dbVal(10) @tt("Automóvel - Passageiro") AutoPass;
+	 @dbVal(11) @tt("Motocicleta - Condutor") MotoCond;
+	 @dbVal(12) @tt("Motocicleta - Passageiro") MotoPass;
+	 @dbVal(13) @tt("Taxi") Taxi;
+	 @dbVal(14) @tt("Motorista Privado") MotoristaPrivado;
+	 @dbVal(15) @tt("Moto-Taxi") MotoTaxi;
+	 @dbVal(99) @tt("Outros") Outros;
  }
+ class MeioTransporte_Tbl extends EnumTable { }
  
-  class MeioTransporte_Tbl extends EnumTable {
-	 
- }
- 
-  @dbNullVal(101) enum Motivo
+  @dbNullVal(101) @tt("Motivo da viagem:") enum Motivo
  {
-	@dbVal(1) Residencia;
-	@dbVal(2)  TrabPrincipal;
-	@dbVal(3) TrabSecundario;
-	@dbVal(4) Negocios;
-	@dbVal(5) EstudoReg;
-	@dbVal(6) EstudoSec;
-	@dbVal(7) Compras;
-	@dbVal(8) Pessoais;
-	@dbVal(9) Refeicao;
-	@dbVal(10) Saude;
-	@dbVal(11) Lazer;
-	@dbVal(12) LevarPessoa;
-	@dbVal(99) Outros;
-	@dbVal(101) NaoRespondeu;
+	@dbVal(1) @tt("Residência") Residencia;
+	@dbVal(2) @tt("Trabalho Principal") TrabPrincipal;
+	@dbVal(3) @tt("Trabalho Secundário") TrabSecundario;
+	@dbVal(4) @tt("Negócios") Negocios;
+	@dbVal(5) @tt("Estudo Regular") EstudoReg;
+	@dbVal(6) @tt("Estudo Outros") EstudoSec;
+	@dbVal(7) @tt("Compras") Compras;
+	@dbVal(8) @tt("Pessoais") Pessoais;
+	@dbVal(9) @tt("Refeição") Refeicao;
+	@dbVal(10) @tt("Saúde") Saude;
+	@dbVal(11) @tt("Lazer") Lazer;
+	@dbVal(12) @tt("Levar outra pessoa") LevarPessoa;
+	@dbVal(99) @tt("Outro") Outros;
+	@dbVal(101) @tt("Não Respondeu") NaoRespondeu;
  }
- 
-  class Motivo_Tbl extends EnumTable {
-	 
- }
+ class Motivo_Tbl extends EnumTable { }
+
  //TODO
- @dbNullVal(3) enum MotivoSemViagem
+ @dbNullVal(3) @tt("Motivo sem viagens:") enum MotivoSemViagem
  {
-	@dbVal(1) NaoRealizou;
-	@dbVal(2) NaoSoube;
-	@dbVal(3) NaoRespondeu;
+	@dbVal(1) @tt("Não Realizou") NaoRealizou;
+	@dbVal(2) @tt("Nao Soube") NaoSoube;
+	@dbVal(3) @tt("Não Respondeu") NaoRespondeu;
  }
+ class MotivoSemViagem_Tbl extends EnumTable { }
  
-  class MotivoSemViagem_Tbl extends EnumTable {
-	 
- }
- 
- @dbNullVal(0) enum OcupacaoDomicilio
+ @dbNullVal(0) @tt("Ocupacao Domicílio") enum OcupacaoDomicilio
  {
-	@dbVal(1) Uni;
-	@dbVal(2) Multi;
-	@dbVal(3) Republica;
-	@dbVal(4) Compartilhado;
-	@dbVal(99) Outros;
+	@dbVal(1) @tt("Unifamiliar")  Uni;
+	@dbVal(2) @tt("Multifamiliar") Multi;
+	@dbVal(3) @tt("República") Republica;
+	@dbVal(4) @tt("Compartilhado") Compartilhado;
+	@dbVal(99) @tt("Outros") Outros;
  }
- 
-  class OcupacaoDomicilio_Tbl extends EnumTable {
-	 
- }
+ class OcupacaoDomicilio_Tbl extends EnumTable { }
 
-
-
- @dbNullVal(0) enum PortadorNecessidadesEspeciais
+ @dbNullVal(0) @tt("Portador de Necessidades Especiais") enum PortadorNecessidadesEspeciais
  {
-	@dbVal(1) NaoAplica;
-	@dbVal(2) Cognitiva;
-	@dbVal(3) Cegueira;
-	@dbVal(4) FisicaTemp;
-	@dbVal(5) FisicaPerm;
-	@dbVal(6) Cadeirante;
-	@dbVal(7) Outros;
+	@dbVal(1) @tt("Não") NaoAplica;
+	@dbVal(2) @tt("Cognitiva") Cognitiva;
+	@dbVal(3) @tt("Cegueira") Cegueira;
+	@dbVal(4) @tt("Fisica Temporária") FisicaTemp;
+	@dbVal(5) @tt("Fisica Permanente") FisicaPerm;
+	@dbVal(6) @tt("Cadeirante") Cadeirante;
+	@dbVal(7) @tt("Outros") Outros;
  }
+ class PortadorNecessidadesEspeciais_Tbl extends EnumTable { }
  
-  class PortadorNecessidadesEspeciais_Tbl extends EnumTable {
-	 
- }
- 
- @dbNullVal(101) enum SetorAtividadeEmpresaPrivada
+ @dbNullVal(101) @tt("Setor de Atividade em Empresa Privada") enum SetorAtividadeEmpresaPrivada
  {
-	 @dbVal(1) Agropecuaria;
-	 @dbVal(2) ConstrucaoCivil;
-	 @dbVal(3) Industria;
-	 @dbVal(4) Comercio;
-	 @dbVal(5) Financeiro;
-	 @dbVal(6) Servicos;
-	 @dbVal(99) Outro;
-	 @dbVal(101) NR;
+	 @dbVal(1) @tt("Agropecuária")  Agropecuaria;
+	 @dbVal(2) @tt("Construção Civil") ConstrucaoCivil;
+	 @dbVal(3) @tt("Indústria") Industria;
+	 @dbVal(4) @tt("Comércio") Comercio;
+	 @dbVal(5) @tt("Financeiro") Financeiro;
+	 @dbVal(6) @tt("Serviços") Servicos;
+	 @dbVal(99) @tt("Outro") Outro;
+	 @dbVal(101) @tt("Não Respondeu") NR;
  }
+ class SetorAtividadeEmpresaPrivada_Tbl extends EnumTable { }
  
- class SetorAtividadeEmpresaPrivada_Tbl extends EnumTable {
-	 
+ @dbNullVal(101) @tt("Setor de Atividade em Empresa Pública") enum SetorAtividadeEmpresaPublica {
+	@dbVal(1) @tt("Administração Federal") AdministracaoFederal;
+	@dbVal(2) @tt("Administração GDF") AdministracaoGDF;
+	@dbVal(3) @tt("Administração Municipal") AdministracaoMunicipal;
+	@dbVal(101) @tt("Não Respondeu") NR;
  }
+ class SetorAtividadeEmpresaPublica_Tbl extends EnumTable { }
  
- @dbNullVal(101) enum SetorAtividadeEmpresaPublica {
-	@dbVal(1) AdministracaoFederal;
-	@dbVal(2) AdministracaoGDF;
-	@dbVal(3) AdministracaoMunicipal;
-	@dbVal(101) NR;
- }
- 
- class SetorAtividadeEmpresaPublica_Tbl extends EnumTable {
-	 
- }
- 
- @dbNullVal(13) enum RendaDomiciliar
+ @dbNullVal(13) @tt("Renda Domiciliar") enum RendaDomiciliar
  {
-	@dbVal(1) Sem;
-	@dbVal(2) De0;
-	@dbVal(3) De440;
-	@dbVal(4) De880;
-	@dbVal(5) De1760;
-	@dbVal(6) De2640;
-	@dbVal(7) De4400;
-	@dbVal(8) De8800;
-	@dbVal(9) De13200;
-	@dbVal(10) De17600;
-	@dbVal(11) De22000;
-	@dbVal(12) De26400;	
-	@dbVal(13) NaoRespondeu;
+	@dbVal(1) @tt("Sem Renda") Sem;
+	@dbVal(2) @tt("Acima de 0 e até 440 reais ") De0;
+	@dbVal(3) @tt("Acima de 440 reais e até 880 reais") De440;
+	@dbVal(4) @tt("Acima de 880 reais e até 1.760 reais") De880;
+	@dbVal(5) @tt("Acima de 1760 reais e até 2.640 reais") De1760;
+	@dbVal(6) @tt("Acima de 2640 reais e até 4.400 reais") De2640;
+	@dbVal(7) @tt("Acima de 4400 reais e até 8.800 reais") De4400;
+	@dbVal(8) @tt("Acima de 8.800 reais e até 13.200 reais") De8800;
+	@dbVal(9) @tt("Acima de 13.200 reais e até 17.600 reais") De13200;
+	@dbVal(10) @tt("Acima de 17.600 reais e até 22.000 reais") De17600;
+	@dbVal(11) @tt("Acima de 22.000 reais e até 26.400 reais") De22000;
+	@dbVal(12) @tt("Acima de 26.400 reais") De26400;	
+	@dbVal(13) @tt("Não Respondeu") NaoRespondeu;
  }
+  class RendaDomiciliar_Tbl extends EnumTable { }
  
-  class RendaDomiciliar_Tbl extends EnumTable {
-	 
- }
- 
- @dbNullVal(0) enum SituacaoFamiliar
+ @dbNullVal(0) @tt("Situação Familiar") enum SituacaoFamiliar
  {
-	@dbVal(1) Responsavel;
-	@dbVal(2) Conjuge;
-	@dbVal(3) Filho;
-	@dbVal(4) Pai;
-	@dbVal(5) Parente;
-	@dbVal(6) Pensionista;
-	@dbVal(7) Agregado;
-	@dbVal(8) EmpregadoResidente;
-	@dbVal(9) ResidenteTemp;
-	@dbVal(99) Outros;
+	@dbVal(1) @tt("Responsável") Responsavel;
+	@dbVal(2) @tt("Cônjuge") Conjuge;
+	@dbVal(3) @tt("Filho/Filha") Filho;
+	@dbVal(4) @tt("Pai/Mãe/Sogros") Pai;
+	@dbVal(5) @tt("Parente") Parente;
+	@dbVal(6) @tt("Pensionista") Pensionista;
+	@dbVal(7) @tt("Agregado") Agregado;
+	@dbVal(8) @tt("Empregado Residente") EmpregadoResidente;
+	@dbVal(9) @tt("Residente Temporário") ResidenteTemp;
+	@dbVal(99) @tt("Outros") Outros;
  }
+ class SituacaoFamiliar_Tbl extends EnumTable { }
  
-  class SituacaoFamiliar_Tbl extends EnumTable {
-	 
- }
- 
- @dbNullVal(101) enum TipoEstacionamento
+ @dbNullVal(101) @tt("Tipo de Estacionamento") enum TipoEstacionamento
  {
-	@dbVal(1) Propria;
-	@dbVal(2) GratuitoViaPublica;
-	@dbVal(3) GratuitoFora;
-	@dbVal(4) EstPagoMensal;
-	@dbVal(5) EstPagoHora;
-	@dbVal(6) NaoEstacionou;
-	@dbVal(99) Outros;
-	@dbVal(101) NaoRespondeu;
+    @dbVal(2) @tt("Gratuito na Via Pública") GratuitoViaPublica;
+    @dbVal(1) @tt("Própria") Propria;
+	@dbVal(3) @tt("Gratuíto fora da Via Pública") GratuitoFora;
+	@dbVal(4) @tt("Estacionamento Pago Mensal") EstPagoMensal;
+	@dbVal(5) @tt("Estacionamento Pago por Hora") EstPagoHora;
+	@dbVal(6) @tt("Não Estacionou") NaoEstacionou;
+	@dbVal(99) @tt("Outros") Outros;
+	@dbVal(101) @tt("Não Respondeu") NaoRespondeu;
  }
+ class TipoEstacionamento_Tbl extends EnumTable { }
  
- class TipoEstacionamento_Tbl extends EnumTable {
-	 
- }
- 
-  
- @dbNullVal(0) enum TipoImovel
+ @dbNullVal(0) @tt("Tipo de Imóvel") enum TipoImovel
  {
-	 @dbVal(1) Apartamento;
-	 @dbVal(2) CasaAlvenaria;
-	 @dbVal(3) AlvenariaInacabada;
-	 @dbVal(4) BarracoComPiso;
-	 @dbVal(5) BarracoSemPiso;
-	 @dbVal(6) PredioComercial;
-	 @dbVal(99) Outros;
+	 @dbVal(1) @tt("Apartamento")  Apartamento;
+	 @dbVal(2) @tt("Casa de alvenaria Acabada") CasaAlvenaria;
+	 @dbVal(3) @tt("Casa de Alvenaria Inacabada") AlvenariaInacabada;
+	 @dbVal(4) @tt("Barraco Com Piso") BarracoComPiso;
+	 @dbVal(5) @tt("Barraco Sem Piso") BarracoSemPiso;
+	 @dbVal(6) @tt("Predio Comercial") PredioComercial;
+	 @dbVal(99) @tt("Outros") Outros;
  }
- 
-  class TipoImovel_Tbl extends EnumTable {
-	 
- }
+  class TipoImovel_Tbl extends EnumTable { }
  
  
  
