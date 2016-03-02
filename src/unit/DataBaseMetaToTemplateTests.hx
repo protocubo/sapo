3c50f0fada86ea0cc6433d12ae8f99cba7b64d6e
Jonas Malaco Filho <jonas@jonasmalaco.com>
2016-03-01 22:30:21 -0300

    Fix: don't build Context.iterate if !tink_template
    Replaces and remnoves -D sapo_sync

--- src/unit/DataBaseMetaToTemplateTests.hx
+++ src/unit/DataBaseMetaToTemplateTests.hx
@@ -1,34 +1,32 @@
 package unit;
 
-import haxe.rtti.Meta;
-import utest.Assert;
 import common.spod.EnumSPOD;
-import common.spod.Familia;
-import common.spod.Modo;
+import sapo.spod.Survey;
 import common.spod.statics.EstacaoMetro;
 import common.spod.statics.LinhaOnibus;
 import common.spod.statics.Referencias;
 import common.spod.statics.UF;
+import haxe.rtti.Meta;
+import utest.Assert;
 
 @:keep
 class DataBaseMetaToTemplateTests {
-	
-    public function test_001_ReadsomeMeta()
+	public function test_001_ReadsomeMeta()
 	{
-        var v = NaoRespondeu;
-        var e = Type.getEnum(v);
-        var cons = Type.enumConstructor(v);
+		var v = NaoRespondeu;
+		var e = Type.getEnum(v);
+		var cons = Type.enumConstructor(v);
 
-        var meta = haxe.rtti.Meta.getFields(e);
-        var cnMeta = Reflect.field(meta, cons);
-        var tmeta:Array<Dynamic> = cnMeta.tempstring;
-        for (text in tmeta) {
-            if (Type.getClassName(Type.getClass(text)) != "String")
-                trace (' $text  not a string!');
-            else
-                trace('v to text = $text');
-        }
-
+		var meta = haxe.rtti.Meta.getFields(e);
+		var cnMeta = Reflect.field(meta, cons);
+		var tmeta:Array<Dynamic> = cnMeta.tempstring;
+		for (text in tmeta) {
+			if (Type.getClassName(Type.getClass(text)) != "String")
+				trace (' $text  not a string!');
+			else
+				trace('v to text = $text');
+		}
+	}
 
 	public function new() {}
 }
