69a14a07a044dd0a8da95153f725f309670b864d
Caio <caio.honma@gmail.com>
2016-03-03 15:56:41 -0300

    Random Survey Gen (needs a structure like PATHTO.N/private/csvs to generate my static classes

--- src/sapo/Context.hx
+++ src/sapo/Context.hx
@@ -1,8 +1,13 @@
 package sapo;
 
 import common.Dispatch;
-import common.spod.EnumSPOD.EstadoPesquisa;
-import common.spod.Survey;
+import common.spod.EnumSPOD;
+import common.spod.Modo;
+import common.spod.Ponto;
+import common.spod.statics.EstacaoMetro;
+import common.spod.statics.LinhaOnibus;
+import common.spod.statics.UF;
+import sapo.spod.Survey;
 import common.Web;
 import common.crypto.Password;
 import common.db.MoreTypes;
@@ -137,6 +142,8 @@ class Context {
 			ticket2.insert();
 			new TicketMessage(ticket2, ford, "Time is an illusion, lunchtime doubly so. ").insert();
 			new TicketMessage(ticket2, arthur, "Very deep. You should send that in to the Reader's Digest. They've got a page for people like you.").insert();
+			
+			surveyGen();
 		} catch (e:Dynamic) {
 			rollback();
 			neko.Lib.rethrow(e);
@@ -148,7 +155,7 @@ class Context {
 	{
 		var surveyorgroup = Group.manager.select($privilege == Privilege.PSurveyor, null, false);
 		var supervisorGroup = Group.manager.select($privilege == Privilege.PSupervisor, null, false);
-		var supervisor = new User(supervisorGroup, new EmailAddress("Sup@sup.com.br"), Supervisor5000);
+		var supervisor = new User(supervisorGroup, new EmailAddress("Sup@sup.com.br"), "Supervisor5000");
 		var i = 0;
 		
 		var userarr = [];
@@ -164,6 +171,7 @@ class Context {
 		}
 		
 		i = 0;
+		var group = [];
 		while (i < 1000)
 		{
 			var s = new Survey();
@@ -173,13 +181,171 @@ class Context {
 			s.lastPageVisited = "END";
 			s.endereco_id = Std.random(9999);
 			s.estadoPesquisa = EstadoPesquisa.Concluida;
-			s.
+			s.bairro = "Asa Centro Oeste";
+			s.logradouro = "Rua GWD";
+			s.numero = "5000";
+			s.lote = "Lote 123";
+			s.macrozona = "Macro 5000";
+			s.municipio = "Brasília";
+			s.old_survey_id = i;
+			s.pin = "ASD-Qer3-qwee";
+			s.syncTimestamp = Date.now().getTime();
+			s.tentativa_id = 1;
+			s.checkCT = false;
+			s.checkSupervisor = false;
+			s.checkSuper = false;
+			s.date_create = DateTools.delta(Date.now(), -1000.0 * 60 * 60 * 24 * Std.random(5));
+			s.date_started = s.date_create;
+			s.date_finished = DateTools.delta(Date.now(), 1000.0 * 60 * 60 * 24 * Std.random(5));
+			s.date_completed = s.date_finished;
+			var g = group[s.user_id];
+			if (g == null)
+				group[s.user_id] = [1, 0];
+			var group1 = group[s.user_id][0];
+			var count = group[s.user_id][1];
+			if (count < 10)
+				group[s.user_id] = [group1, count + 1];
+			else
+				group[s.user_id] = [group1 + 1, 1];
+			s.group = group[s.user_id][0];
+			s.insert();
 			
+			var f = new Familia();
+			f.aguaEncanada = Type.createEnumIndex(AguaEncanada, Std.random(2));
+			f.anoVeiculoMaisRecente = Type.createEnumIndex(AnoVeiculoMaisRecente, Std.random(8));
+			f.banheiros = Std.random(3);
+			f.bicicletas = Std.random(1);
+			f.condicaoMoradia = Type.createEnumIndex(CondicaoMoradia, Std.random(7));
+			f.date = DateTools.delta(s.date_create, 1000 * 60 * Std.random(60));
+			f.empregadosDomesticos = Type.createEnumIndex(EmpregadosDomesticos, Std.random(4));
+			f.isDeleted = false;
+			f.isEdited = 0;
+			f.motos = Std.random(3);
+			f.nomeContato = "Red Herring " + i;
+			f.numeroResidentes = Std.random(4);
+			f.ocupacaoDomicilio = Type.createEnumIndex(OcupacaoDomicilio, Std.random(4));
+			f.old_id = i;
+			f.old_survey_id = s.old_survey_id;
+			f.quartos = Std.random(4);
+			f.recebeBolsaFamilia = randomBool();
+			f.rendaDomiciliar = Type.createEnumIndex(RendaDomiciliar, Std.random(12));
+			f.ruaPavimentada_id = randomBool();
+			f.survey = s;
+			f.syncTimestamp = s.syncTimestamp;
+			f.telefoneContato = "9999-9999";
+			f.tentativa_id = 1;
+			f.tipoImovel = Type.createEnumIndex(TipoImovel, Std.random(7));
+			f.tvCabo_id = randomBool();
+			f.vagaPropriaEstacionamento_id = randomBool();
+			f.veiculos = Std.random(4);
+			f.insert();
+			
+			var j = 0;
+			while (j < f.numeroResidentes)
+			{
+				var m = new Morador();
+				m.atividadeMorador = Type.createEnumIndex(AtividadeMorador, Std.random(15));
+				m.date = DateTools.delta(f.date, 1000 * 60 * Std.random(60));
+				m.familia = f;
+				m.genero_id = Std.random(1);
+				m.grauInstrucao = Type.createEnumIndex(GrauInstrucao, Std.random(12));
+				m.idade = Type.createEnumIndex(Idade, Std.random(14));
+				m.isDeleted = false;
+				m.isEdited = 0;
+				m.motivoSemViagem = Type.createEnumIndex(MotivoSemViagem, Std.random(3));
+				m.nomeMorador = "Zacarias José " + i;
+				m.old_id = i + j;
+				m.old_survey_id = s.old_survey_id;
+				m.portadorNecessidadesEspeciais = Type.createEnumIndex(PortadorNecessidadesEspeciais, Std.random(7));
+				m.possuiHabilitacao_id = randomBool();
+				m.proprioMorador_id = randomBool();
+				m.quemResponde = null;
+				m.setorAtividadeEmpresaPrivada = Type.createEnumIndex(SetorAtividadeEmpresaPrivada, Std.random(8)); 
+				m.setorAtividadeEmpresaPublica = Type.createEnumIndex(SetorAtividadeEmpresaPublica, Std.random(4));
+				m.situacaoFamiliar = Type.createEnumIndex(SituacaoFamiliar, Std.random(10));
+				m.survey = s;
+				m.syncTimestamp = s.syncTimestamp;
+				m.insert();
+				
+				var n = 0;
+				while (n < Std.random(4))
+				{
+					var p = new Ponto();
+					p.city_id = Std.random(10000);
+					p.complement_id = Std.random(1000);
+					p.complement_two_id = Std.random(1000);
+					p.complement2_str = "random";
+					p.copiedFrom = null;
+					p.date = DateTools.delta(m.date, 1000 * 60 * Math.random());
+					p.isDeleted = false;
+					p.isEdited = 0;
+					p.isPontoProx = randomBool();
+					p.morador = m;
+					p.motivo = Type.createEnumIndex(Motivo, Std.random(14));
+					p.motivoOutraPessoa = Type.createEnumIndex(Motivo, Std.random(14));
+					p.old_id = Std.random(4);
+					p.old_survey_id = s.old_survey_id;
+					p.pontoProx = null;
+					p.ref = null;
+					p.ref_str = "Random place ";
+					p.regadm_id = Std.random(9999);
+					p.street_id = Std.random(9999);
+					p.survey = s;
+					p.syncTimestamp = m.syncTimestamp;
+					p.tempo_chegada = Std.random(24)  + ":" + Std.random(60);
+					p.tempo_saida = Std.random(24) + ":" + Std.random(60);
+					p.uf = UF.manager.get(1);
+					p.insert();
+					
+					var o = 0;
+					while ( o < Std.random(2) && n%2 == 0)
+					{
+						var mo = new Modo();
+						mo.date = DateTools.delta(p.date, 1000 * 60 * Std.random(40));
+						mo.estacaoDesembarque = EstacaoMetro.manager.get(Std.random(20));
+						mo.estacaoEmbarque = EstacaoMetro.manager.get(Std.random(20));
+						mo.firstpoint = Ponto.manager.get(Manager.cnx.lastInsertId());
+						mo.secondpoint = p;
+						mo.formaPagamento = Type.createEnumIndex(FormaPagamento, Std.random(7));
+						mo.isDeleted = false;
+						mo.isEdited = 0;
+						mo.linhaOnibus = LinhaOnibus.manager.get(Std.random(50));
+						mo.meiotransporte = Type.createEnumIndex(MeioTransporte, Std.random(16));
+						mo.morador = m;
+						mo.naoRespondeu = randomBool();
+						mo.naoSabe = randomBool();
+						mo.old_id = i + j + o;
+						mo.old_morador_id = m.old_id;
+						mo.old_survey_id = s.old_survey_id;
+						mo.survey = s;
+						mo.syncTimestamp = s.syncTimestamp;
+						mo.tipoEstacionamento = Type.createEnumIndex(TipoEstacionamento, Std.random(8));
+						mo.valorViagem = Math.random() * Std.random(40);
+						mo.insert();
+						
+						
+						o++;
+					}
+					n++;
+				}
+				j++;
+			}
+			
+			if (i % 100 == 0)
+				Manager.cnx.commit();
+				
 			i++;
+			
 		}
 		
+		Manager.cnx.commit();
+		
 	}
-
+	static function randomBool() : Null<Bool>
+	{
+		var v = Std.random(3);
+		return ((v == 2) ? null : (v == 1));
+	}
 	public static function init()
 	{
 		InitDB.run();
