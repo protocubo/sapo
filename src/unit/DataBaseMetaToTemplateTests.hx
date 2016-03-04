a4c0133ed1833af3142aedb3a6cb43d592d3e617
Arthur Szasz <arthur@elebeta.com.br>
2016-03-04 15:09:32 -0300

    Add vars dom, fam and user to Survey(s).tt

--- src/unit/DataBaseMetaToTemplateTests.hx
+++ src/unit/DataBaseMetaToTemplateTests.hx
@@ -1,31 +1,15 @@
 package unit;
 
 import common.spod.EnumSPOD;
-import sapo.spod.Survey;
-import common.spod.statics.EstacaoMetro;
-import common.spod.statics.LinhaOnibus;
-import common.spod.statics.Referencias;
-import common.spod.statics.UF;
-import haxe.rtti.Meta;
 import utest.Assert;
 
 @:keep
 class DataBaseMetaToTemplateTests {
 	public function test_001_ReadsomeMeta()
 	{
-		var v = NaoRespondeu;
-		var e = Type.getEnum(v);
-		var cons = Type.enumConstructor(v);
-
-		var meta = haxe.rtti.Meta.getFields(e);
-		var cnMeta = Reflect.field(meta, cons);
-		var tmeta:Array<Dynamic> = cnMeta.tt;
-		for (text in tmeta) {
-			if (Type.getClassName(Type.getClass(text)) != "String")
-				trace (' $text  not a string!');
-			else
-				trace('v to text = $text');
-		}
+		Assert.equals("Pré-Escolar", sapo.view.Util.enumText(PreEscolar));
+		Assert.equals("Até 1 vez por mês", sapo.view.Util.enumText(Vez1Mes));
+		Assert.equals("", sapo.view.Util.enumText(Ganguashare));
 	}
 	public function new() {}
 }
