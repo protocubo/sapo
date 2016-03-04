711dafcdf4e54239958ecc5939c94b453b5cdd18
Arthur Szasz <arthur@elebeta.com.br>
2016-03-04 17:01:04 -0300

    Fix test enumtext

--- src/unit/DataBaseMetaToTemplateTests.hx
+++ src/unit/DataBaseMetaToTemplateTests.hx
@@ -9,7 +9,9 @@ class DataBaseMetaToTemplateTests {
 	{
 		Assert.equals("Pré-Escolar", sapo.view.Util.enumText(PreEscolar));
 		Assert.equals("Até 1 vez por mês", sapo.view.Util.enumText(Vez1Mes));
-		Assert.equals("", sapo.view.Util.enumText(Ganguashare));
+		Assert.equals("DEMissing", sapo.view.Util.enumText(common.Dispatch.DispatchError.DEMissing));
+		Assert.equals("DEMissingParam",sapo.view.Util.enumText(common.Dispatch.DispatchError.DEMissingParam("nopog")));
+		//Assert.raises(sapo.view.Util.enumText.bind(common.Dispatch.DispatchError.DEMissingParam("nopog")));
 	}
 	public function new() {}
 }
